fc7fd819ae24c3f1885fe83732d3ffd2fea24c48
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-07 23:58:13 +0530

    Got outline.

--- logos/analyze.py
+++ logos/analyze.py
@@ -17,6 +17,12 @@ import numpy as np
 import cv2
 import itertools
 
+def dist(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    d = ((x1-x2)**2 + (y1-y2)**2)**0.5
+    return d
+
 def path_sort( path ):
     newpath = [ path[0] ]
     path.pop(0)
