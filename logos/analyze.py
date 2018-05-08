33aa91c4faece5d269487ddeec9f9fdc31cfec70
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-08 12:16:36 +0530

    Somewhat better but we need corner detection.

--- logos/analyze.py
+++ logos/analyze.py
@@ -24,8 +24,22 @@ def dist(p1, p2):
     return d
 
 def min_nonzero(vec):
-    return min([(d,x) for (d,x) in vec if d >= 1])
+    try:
+        return min([(d,x) for (d,x) in vec if d > 0])
+    except Exception as e:
+        return min(vec)
 
+def smooth_but_preserve_corners(path):
+    path = path_sort( path )
+    newpath = [ path[0] ]
+    t = 0
+    for p in path[1:]:
+        if abs(p[0] - newpath[-1][0]) < t or abs(p[1]-newpath[-1][1])< t:
+            print( '.', end = '' )
+            continue
+        newpath.append( p )
+    print('')
+    return newpath
 
 def path_sort( path ):
     newpath = [ path[0] ]
@@ -38,14 +52,15 @@ def path_sort( path ):
 
 def main( ):
     img = cv2.imread( './logo.png', 0 )
+    img = cv2.bilateralFilter(img, 9, 75, 75)
     img1 = cv2.Canny( img, 50, 200 )
-    n, temp = cv2.connectedComponents(img1, 8)
+    n, temp = cv2.connectedComponents(img1, 4)
     new = np.zeros_like(temp)
 
     for l in range(1,int(temp.max())+1):
         p1 = np.where( temp == l )
         with open('path%d.txt' % l, 'w' ) as f:
-            for x, y in path_sort(list(zip(*p1))):
+            for x, y in smooth_but_preserve_corners(list(zip(*p1))):
                 new[x,y]=10*l
                 f.write('%g %g\n' % (y,-x))
         print('Wrote labeled path to path%d.txt' % l)
